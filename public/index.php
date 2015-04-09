fc6af7bb8eed3a29f3335b32a4747bd8e6c28114
Jamie Shields <jayshields@gmail.com>
2015-04-09 11:12:05 +0100

    Replace Doctrine with PDO & tidy up/commenting

--- public/index.php
+++ public/index.php
@@ -3,23 +3,35 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-  'db.options' => array(
-    'driver' => 'pdo_mysql',
-    'host' => 'localhost',
-    'dbname' => 'example',
-    'user' => 'root',
-    'password' => '',
-    'driverOptions' => array(\PDO::ATTR_EMULATE_PREPARES => false)
-  ),
-));
+//Initialise the database connection
+$app['db'] = function()
+{
+  $host = 'localhost';
+  $db_name = 'example';
+  $user = 'root';
+  $pass = '';
 
+  return new \PDO(
+    "mysql:host={$host};dbname={$db_name}",
+    $user,
+    $pass,
+    array(\PDO::ATTR_EMULATE_PREPARES => false));
+};
+
+//Set up the route for viewing the HTML frontend
 $app->get('/', function() use($app) {
-  return file_get_contents(__DIR__."/../resources/views/index.html");
+  return file_get_contents(__DIR__.'/../resources/views/index.html');
 });
 
+//Set up the route for accessing a page of the result set
 $app->get('/data/page/{page_num}/{rows_per_page}', function($page_num, $rows_per_page) use($app) {
-  $result = $app['db']->fetchAll("
+
+  //Put the limit parameters into variables
+  $start = ((int)$page_num - 1) * (int)$rows_per_page;
+  $total_rows = (int)$rows_per_page;
+
+  //Prepare and execute the query
+  $stmt = $app['db']->prepare('
   SELECT
     `name`
   FROM
@@ -27,19 +39,28 @@ $app->get('/data/page/{page_num}/{rows_per_page}', function($page_num, $rows_per
   ORDER BY
     `name`
   LIMIT
-    ?, ?",
-  array(((int)$page_num - 1) * (int)$rows_per_page, (int)$rows_per_page));
+    :from, :total_rows');
+  $stmt->bindParam('from', $start);
+  $stmt->bindParam('total_rows', $total_rows);
+  $stmt->execute();
 
+  //Return the rows as JSON
+  $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
   return $app->json($result);
 });
 
+//Set up the route for accessing the total rows in the result set
 $app->get('/data/countrows', function() use($app) {
-  $result = $app['db']->fetchAssoc("
+
+  //Execute the query
+  $stmt = $app['db']->query('
   SELECT
     COUNT(`id`) AS `total_rows`
   FROM
-    `people`");
+    `people`');
 
+  //Return the rows as JSON
+  $result = $stmt->fetch(\PDO::FETCH_ASSOC);
   return $app->json($result);
 });
 
